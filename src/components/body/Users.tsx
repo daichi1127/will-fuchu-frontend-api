7a4c8041e3bb1093726fb87d0ab8b76f389a951e
Daichi Murakami <14b1205@gmail.com>
2021-10-19 21:27:04 +0900

    fix Users.tsx

--- src/components/body/Users.tsx
+++ src/components/body/Users.tsx
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import React, {useState, useEffect} from 'react';
-import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import {
